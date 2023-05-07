430aae76e885b0e65924cda300d15281651bfe62
MoBsTe <valery.lobach@gail.com>
2023-05-07 08:36:06 +0200

    add input add todo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Keyboard, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -13,6 +13,15 @@ export default function App() {
           <Task text="Mleko 3" />
         </View>
       </View>
+
+      <KeyboardAvoidingView style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task'} />
+        <TouchableOpacity onPress={() => handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -34,5 +43,35 @@ const styles = StyleSheet.create({
   },
   items: {
     margin: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText: {
   }
+
 });
